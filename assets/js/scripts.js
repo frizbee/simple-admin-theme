f855f4008fb6838627f325b0fca06e472c3edc76
frizbee <mail2andrewa@gmail.com>
2017-11-16 20:57:33 +0700

    added some js

---
+++ assets/js/scripts.js
@@ -0,0 +1,20 @@
+/**
+#
+#
+# Scripts for Simple Admin
+#
+*/
+
+/**
+ * Document height and width
+ */
+var width, height
+width = window.outerWidth
+height = window.outerHeight
+
+
+// Sets minimum height
+// of the layout
+var content = document.querySelector('.content-wrapper')
+//content.style.minHeight = (height - 179) + 'px'
+content.style.minHeight = (height - 134) + 'px'
